0337524361b1ac20aedab803bb813bface31d0c7
Barthecl <clement0122@gmail.com>
2016-11-11 16:07:16 +0100

    testsim

---
+++ 2A_POO_robots2016/src/tests/TestSimulateurStatique.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class TestSimulateurStatique {
+	
+	public static void main (String[] fichier){
+		Simulateur = new Simulateur (fichier);
+		Simulateur.Afficher;
+		public static void Afficher {
+			
+		}
+	}
+}
