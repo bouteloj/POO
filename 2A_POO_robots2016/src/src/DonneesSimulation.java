a9f9c7108d75c38cbd5357070f1133067f0a9870
Barthecl <clement0122@gmail.com>
2016-11-11 18:59:40 +0100

    Travail du vendredi

--- 2A_POO_robots2016/src/src/DonneesSimulation.java
+++ 2A_POO_robots2016/src/src/DonneesSimulation.java
@@ -3,7 +3,7 @@ package src;
 import java.util.LinkedList;
 
 public class DonneesSimulation {
-	private Carte map;
+	public Carte map;
 	private LinkedList<Robot> robots;
 	private LinkedList<Incendie> incendies;
 	
