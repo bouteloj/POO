31b4bca843fb38195df499014f148415ea6701ab
MERIAM <MERIAM@MERIAM-PC>
2016-11-12 18:09:38 +0100

    verser eau

--- 2A_POO_robots2016/src/events/VerserEau.java
+++ 2A_POO_robots2016/src/events/VerserEau.java
@@ -2,6 +2,7 @@ package events
 
 public class VerserEau extends Evenement{
 	private Incendie incendie;
+	private LinkedList<Incendie> listeIncendies;
 	private Robot robot;
 	private Simulateur simul;
 	
@@ -10,13 +11,30 @@ public class VerserEau extends Evenement{
 		super(date);
 		this.incendie=incendie;
 		this.robot=robot;
+		this.listeIncendies = listeDIncendies;
 		this.simul=simul;
 	}
 	
 	@Override
 	public void execute(){
-		if ((this.incentie.getVerser != 0 ) && (robot.deverserEau(incendie))){
-			simul.ajouteEvenement(new VerserEau(date + robot.tempsIntervention, incendie, robot, simul));
+		int e;
+		e= this.robot.quantiteIntervention();
+		
+		if (incendie.getVerser() > 0) {
+            this.robot.deverserEau(e);
+			incendie.Verser(e);
+		} else {
+			System.out.println(robot.getClass() + "vidé");
+			
+		}
+
+		if (incendie.getVerser() <= 0) {
+			listeIncendies.remove(incendie);
+		}
+		
+	}
+		//if ((this.incentie.getVerser != 0 ) && (robot.deverserEau(incendie))){
+			//simul.ajouteEvenement(new VerserEau(date + robot.tempsIntervention, incendie, robot, simul));
 		}
 		
 		else  {
