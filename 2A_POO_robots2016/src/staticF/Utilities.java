b5d3ac5b57d0646e13fd6ac5d98abdbe785db1ac
julien bouteloup <julien.bouteloup@ensimag.grenoble-inp.fr>
2016-11-13 13:13:48 +0100

    efdij

---
+++ 2A_POO_robots2016/src/staticF/Utilities.java
@@ -0,0 +1,26 @@
+package staticF;
+
+import java.util.LinkedList;
+
+import robots.Robot;
+import simulateur.Simulateur;
+import src.Case;
+import src.Direction;
+
+public class Utilities {
+	
+	private int poids(Simulateur simul, Robot rob, LinkedList<Direction> list){
+		
+	}
+
+		
+	public static LinkedList<Direction> plusCourtChemin(Simulateur simul, Robot rob, Case c){
+		LinkedList<Direction> returnlist=new LinkedList<Direction>();
+			
+		
+		
+			
+		return returnlist;
+			
+	}
+}
